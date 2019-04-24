d63894526036800c26f1e9f529606408534b327d
softwareguycoder <softwareguycoder@gmail.com>
2019-04-24 09:06:15 -0400

    Added skeleton of main library source and header files

---
+++ include/uuid_core.h
@@ -0,0 +1,9 @@
+// uuid_core.h - Defines the public interface to the uuid_core library
+//
+
+#ifndef __UUID_CORE_H__
+#define __UUID_CORE_H__
+
+
+
+#endif /* __UUID_CORE_H__ */
---
+++ include/uuid_core_symbols.h
@@ -0,0 +1,8 @@
+// uuid_core_symbols.h - #define'd constants
+
+#ifndef __UUID_CORE_SYMBOLS_H__
+#define __UUID_CORE_SYMBOLS_H__
+
+
+
+#endif /* __UUID_CORE_SYMBOLS_H__ */
