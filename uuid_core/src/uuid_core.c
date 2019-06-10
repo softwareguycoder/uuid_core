646983b0b7ba8dc5e61c4041aafefe8bc043af37
softwareguycoder <softwareguycoder@gmail.com>
2019-06-10 07:51:39 -0400

    Call ThrowArgumentException from UUIDFromString function

--- uuid_core/src/uuid_core.c
+++ uuid_core/src/uuid_core.c
@@ -7,8 +7,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Internally-used functions
 
-
-
 ///////////////////////////////////////////////////////////////////////////////
 // Externally-exposed functions
 
@@ -16,18 +14,18 @@
 // AreEqual function
 
 BOOL AreUUIDsEqual(UUID* pUUID1, UUID* pUUID2) {
-    if (!IsUUIDValid(pUUID1)) {
-        return FALSE;
-    }
+  if (!IsUUIDValid(pUUID1)) {
+    return FALSE;
+  }
 
-    if (!IsUUIDValid(pUUID2)) {
-        return FALSE;
-    }
+  if (!IsUUIDValid(pUUID2)) {
+    return FALSE;
+  }
 
-    /* the two UUID variables passed to this function
-     * have the same value if the uuid_compare function
-     * returns zero. */
-    return uuid_compare(*pUUID1, *pUUID2) == 0;
+  /* the two UUID variables passed to this function
+   * have the same value if the uuid_compare function
+   * returns zero. */
+  return uuid_compare(*pUUID1, *pUUID2) == 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -50,60 +48,60 @@ void CopyUUID(UUID* pDestUUID, UUID* pSrcUUID)
 // GenerateNewUUID function
 
 void GenerateNewUUID(UUID* pUUID) {
-    // Must have an address in memory where
-    // to copy the generated value to
-    if (pUUID == NULL || *pUUID == NULL) {
-        ThrowUUIDNullException();
-    }
+  // Must have an address in memory where
+  // to copy the generated value to
+  if (pUUID == NULL || *pUUID == NULL) {
+    ThrowUUIDNullException();
+  }
 
-    // generate
-    uuid_generate_time_safe(*pUUID);
+  // generate
+  uuid_generate_time_safe(*pUUID);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // IsUUIDValid function
 
 BOOL IsUUIDValid(UUID* pUUID) {
-    // Must have an address in memory where the UUID
-    // to be checked is sitting
-    if (pUUID == NULL || *pUUID == NULL) {
-        return FALSE; // NULL reference is not a valid UUID
-    }
+  // Must have an address in memory where the UUID
+  // to be checked is sitting
+  if (pUUID == NULL || *pUUID == NULL) {
+    return FALSE; // NULL reference is not a valid UUID
+  }
 
-    return !uuid_is_null(*pUUID);
+  return !uuid_is_null(*pUUID);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // UUIDFromString function
 
 void UUIDFromString(const char* pszUUID, UUID* pOutputUUID) {
-    if (IsNullOrWhiteSpace(pszUUID)) {
-        ThrowUUIDNullException();
-    }
+  if (IsNullOrWhiteSpace(pszUUID)) {
+    ThrowArgumentException("pszUUID");
+  }
 
-    if (pOutputUUID == NULL || *pOutputUUID == NULL) {
-        ThrowUUIDNullException();
-    }
+  if (pOutputUUID == NULL || *pOutputUUID == NULL) {
+    ThrowUUIDNullException();
+  }
 
-    uuid_parse(pszUUID, *pOutputUUID);
+  uuid_parse(pszUUID, *pOutputUUID);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // UUIDToString function
 
 char* UUIDToString(UUID* pUUID) {
-    if (!IsUUIDValid(pUUID)) {
-        ThrowUUIDNullException();
-    }
+  if (!IsUUIDValid(pUUID)) {
+    ThrowUUIDNullException();
+  }
 
-    char* pszResult = (char*) malloc(UUID_SIZE*sizeof(char));
-    if (pszResult == NULL) {
-        ThrowUUIDInvalidException(FAILED_ALLOC_UUID_STORAGE);
-    }
-    memset(pszResult, 0, UUID_SIZE*sizeof(char));
+  char* pszResult = (char*) malloc(UUID_SIZE * sizeof(char));
+  if (pszResult == NULL) {
+    ThrowOutOfMemoryException(FAILED_ALLOC_UUID_STORAGE);
+  }
+  memset(pszResult, 0, UUID_SIZE * sizeof(char));
 
-    // unparse (to string)
-    uuid_unparse(*pUUID, pszResult);
+  // unparse (to string)
+  uuid_unparse(*pUUID, pszResult);
 
-    return pszResult;
+  return pszResult;
 }
